5fbccf9e235bc241d9553e62150a4e4c8d71ef3c
Andy Guo <xiao_0429@126.com>
2019-05-26 07:49:46 +0800

    add comments

--- linux-0.12/linux-0.12/mm/page.s
+++ linux-0.12/linux-0.12/mm/page.s
@@ -12,20 +12,20 @@
 .globl page_fault
 
 page_fault:
-	xchgl %eax,(%esp)
+	xchgl %eax,(%esp) # 将错误码交换到 eax中 
 	pushl %ecx
 	pushl %edx
 	push %ds
 	push %es
 	push %fs
-	movl $0x10,%edx
+	movl $0x10,%edx # 修正 ds/es/fs等段寄存器值,志向内核
 	mov %dx,%ds
 	mov %dx,%es
 	mov %dx,%fs
-	movl %cr2,%edx
+	movl %cr2,%edx  # 将 cr2 寄存器,即页错误地址 放入edx,然后压入栈
 	pushl %edx
 	pushl %eax
-	testl $1,%eax
+	testl $1,%eax   # 错误码最低位为1,则为写保护,否则为缺页
 	jne 1f
 	call do_no_page
 	jmp 2f
